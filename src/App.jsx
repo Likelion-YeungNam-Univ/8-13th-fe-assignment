dec8391656d25c4d33cce384470090bf0bbb5e78
hyeji-neee <tlsgmlsk123@naver.com>
2025-05-13 01:22:05 +0900

    style: 기본 레이아웃 스타일 적용

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,29 @@
-import React from "react";
-import Header from "./Header";
-import WatchedList from "./WatchedList";
-import MovieList from "./MovieList";
-import WillWatchList from "./WillWatchList";
 import Footer from "./Footer";
+import Header from "./Header";
+import MovieList from "./MovieList";
+import WatchedList from "./WatchedList";
+import WillWatchList from "./WillWatchList";
 
 const App = () => {
   return (
-    <div>
+    <div className="flex flex-col min-h-screen">
       <Header />
-      <WatchedList />
-      <MovieList />
-      <WillWatchList />
+      <div className="flex flex-1">
+        {/* Left */}
+        <div className="w-1/5 bg-gray-100">
+          <WatchedList />
+        </div>
+
+        {/* Center */}
+        <div className="w-3/5">
+          <MovieList />
+        </div>
+
+        {/* Right */}
+        <div className="w-1/5 bg-gray-100">
+          <WillWatchList />
+        </div>
+      </div>
       <Footer />
     </div>
   );
