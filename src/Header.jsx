4fdb1654efe43d2b0ad075bf4247ae9c0051f48d
hyeji-neee <tlsgmlsk123@naver.com>
2025-05-12 23:45:54 +0900

    style: Header, Footer 컴포넌트 스타일 적용

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <div className="bg-gray-800 text-white p-5 text-center">
+      <h1 className="text-3xl font-bold">Movie List</h1>
+    </div>
+  );
 };
 
 export default Header;
